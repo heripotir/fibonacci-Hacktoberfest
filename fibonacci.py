17ad6839d3eaadc8718b4c9d22ff6594d9f3e34f
Manuel Chimal <manuelchimal@iMac-de-Manuel.local>
2019-10-02 18:17:00 -0500

    fibonacci

--- fibonacci.py
+++ fibonacci.py
@@ -1,17 +1,30 @@
-def fibonacci(n):
-    """a very bad implementation for finding the n'th element of the fibonacci sequence."""
-    if n==0:
-        print("ULAN xd")
-    elif n==1 or n==2:
-        return 1
-    else:
-        return fibonacci(n-1) + fibonacci(n-2)
+import itertools
 
-def fibonacci_seq(*args):
-    """finds a subsequence of the fibonacci sequence using the fibonacci function"""
-    if len(args) == 1:
-        return [fibonacci(n) for n in range(1,args[0]+1)]
-    elif len(args) == 2:
-        return [fibonacci(n) for n in range(args[0], args[1]+1)]
-    else:
-        print("ulan")
+def second_order(p, q, r, initial_values):
+    """
+    Fibonacci numbers follow a homogeneous linear 
+    recurrence relation with constant coefficients
+    Fn = Fn-1 + Fn-2
+    https://en.wikipedia.org/wiki/Recurrence_relation
+    with a solution defined by 
+    s(n) = p * s(n-1) + q * s(n-2) + r.
+    where p, q and r are constants. 
+    To Fibonacci sequence:
+    p = 1
+    q = 1
+    r = 0
+    And de initial conditions 
+    F0 = 0 
+    F1 = 1
+    """
+    intermediate = itertools.accumulate(
+        itertools.repeat(initial_values),
+        lambda s, _: (s[1], p*s[1] + q*s[0] + r)
+    )
+    return map(lambda x: x[0], intermediate)
+
+if __name__ == "__main__":
+    n = int(input('n: '))
+    fibonacci = second_order(p=1, q=1, r=0, initial_values=(0, 1))
+    print(list(next(fibonacci) for _ in range(n)))
+
