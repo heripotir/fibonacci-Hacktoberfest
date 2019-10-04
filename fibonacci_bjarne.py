65bcca5172d52c83afc92ce3afec78f8a896a32a
Mustafa Ozan Alpay <frozsgy@gmail.com>
2019-10-04 16:03:11 +0300

    Faster horrible implementation
    Specifically designed for python lovers, with a bit of Bjarne sauce.

---
+++ fibonacci_bjarne.py
@@ -0,0 +1,9 @@
+import os
+
+sl = "#include<iostream>\nusing namespace std;int fibonacci(int n){if(n==0){cout<<\"ULAN xd\"<<endl;return 0;}else if(n==1||n==2){return 1;}else{return fibonacci(n-1)+fibonacci(n-2);}}int main(){std::ios::sync_with_stdio(false);int fn;cout<<\"Enter which Fibonacci number you'd like to have: \"<<endl;cin>>fn;cout<<fibonacci(fn)<<endl;cout<<\"DIE POTATO DIE\"<<endl;return 0;}"
+f = open("temp.cpp","w+")
+f.write(sl)
+f.close()
+os.system("g++ temp.cpp -o koluacik")
+os.system("./koluacik")
+os.system("rm temp.cpp koluacik")
