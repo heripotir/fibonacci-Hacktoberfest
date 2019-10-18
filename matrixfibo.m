93a7dd4f654a4a602799cad2b1de555d44386dbf
Seral Buse Atak <44511062+heripotir@users.noreply.github.com>
2019-10-18 22:41:06 +0300

    Update and rename mfibo.m to matrixfibo.m

--- matrixfibo.m
+++ matrixfibo.m
@@ -1,4 +1,5 @@
-function y = mfibo(x)
+function y = matrixfibo(x)
+%matrix based implementated fibonacci function
 %useful information: https://medium.com/future-vision/fibonacci-sequence-algorithm-5eebae4e85be
 k=([ 0 1 ; 1 1 ]^x)*[0 ; 1 ];
 y=k(1,1);
